43bcc5fb0ca47c2bf81a1b531129d329f7633e0f
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-31 21:45:11 -0400

    Add comment

--- api-handler/pages/api/multiply.ts
+++ api-handler/pages/api/multiply.ts
@@ -11,7 +11,10 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (isNaN(parsedNumber)) {
         return res.status(400).json({ error: 'Invalid number' });
     }
-
+    // SEND TO DATABASE AND CHECK IF THE NUMBER RECEIVED (USER ID) HAS A COUNT LESS THAN 10
+    // CHECK USER -- ARE THEY IN DB?
+    // IF YES, UPDATE COUNT BY ONE
+    // IF NO, ADD USER WITH COUNT = 1
     const result = parsedNumber * 2;
     return res.status(200).json({ result });
 }
